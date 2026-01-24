40fefb6dd5a4332bad3cf6d1f188244980994503
Siddhi1432 <siddhirmane14@gmail.com>
2026-01-24 16:03:05 +0530

    Task 3: Learned variables and basic data types in Kotlin

---
+++ basics/Variables.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val name: String = "Siddhi"
+    var age: Int = 21
+    val height: Double = 5.0
+    val isStudent: Boolean = true
+
+    println("Name: $name")
+    println("Age: $age")
+    println("Height: $height")
+    println("Is Student: $isStudent")
+
+    age += 1
+    println("Updated age: $age")
+}
